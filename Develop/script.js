34c36ca818c9bfaef3562b4a77d51f1545e2ef84
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-10-28 16:13:39 -0500

    defining initial variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,28 @@
+// Defining Variables //
+var numbers = "0123456789".split("");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var sChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+
+
+// Defining Alerts, Prompts & Confirms
+var greet = alert ("Let's get you a password shall we?");
+var Q = prompt ("How long would you like your password to be?");
+var numQuest = confirm ("Would you like your password to include Numbers?");
+var lowQuest = confirm ("Would you like your password to include Lowercase Letters?");
+var UpQuest = confirm ("Would you like your password to include Uppercase Letters?");
+var scQuest = confirm ("Would you like your password to include Special Characters?");
+
+
+
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
